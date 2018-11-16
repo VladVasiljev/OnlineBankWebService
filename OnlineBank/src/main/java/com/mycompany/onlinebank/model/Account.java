cfda90a9af08d550998c780268c70b0f5314ecad
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2018-11-16 11:32:20 +0000

    Account class done

--- OnlineBank/src/main/java/com/mycompany/onlinebank/model/Account.java
+++ OnlineBank/src/main/java/com/mycompany/onlinebank/model/Account.java
@@ -5,10 +5,80 @@
  */
 package com.mycompany.onlinebank.model;
 
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Vladislavs Vasiljevs
  */
+@XmlRootElement
 public class Account {
+    private String firstName,lastName,sortCode,accountNumber;
+    private double currentBalance;
+    private Date accountCreationDate;
+
+    public Account(String firstName, String lastName, String sortCode, String accountNumber, double currentBalance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.sortCode = sortCode;
+        this.accountNumber = accountNumber;
+        this.currentBalance = currentBalance;
+        this.accountCreationDate = new Date();
+    }
+
+    public Account() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(String sortCode) {
+        this.sortCode = sortCode;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setCurrentBalance(double currentBalance) {
+        this.currentBalance = currentBalance;
+    }
+
+    public Date getAccountCreationDate() {
+        return accountCreationDate;
+    }
+
+    public void setAccountCreationDate(Date accountCreationDate) {
+        this.accountCreationDate = accountCreationDate;
+    }
+    
+    
+    
     
 }
