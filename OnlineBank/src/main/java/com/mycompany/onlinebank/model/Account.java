7b7a6d31a734ea5dcccb6ee2151bf8f7517c9e55
pkinsella32 <paul.kinsella03@gmail.com>
2018-11-22 11:57:13 +0000

    Update Account.java

--- OnlineBank/src/main/java/com/mycompany/onlinebank/model/Account.java
+++ OnlineBank/src/main/java/com/mycompany/onlinebank/model/Account.java
@@ -5,92 +5,56 @@
  */
 package com.mycompany.onlinebank.model;
 
-import java.util.Date;
-import javax.xml.bind.annotation.XmlRootElement;
-
-/**
- *
- * @author Vladislavs Vasiljevs
- */
-@XmlRootElement
 public class Account {
-    private String firstName,lastName,sortCode,accountNumber,accountType;
-    private double currentBalance;
-    private Date accountCreationDate;
-
-    public Account(String firstName, String lastName, String sortCode, String accountNumber,String accountType, double currentBalance) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.sortCode = sortCode;
-        this.accountNumber = accountNumber;
-        this.accountType = accountType;
-        this.currentBalance = currentBalance;
+	
+	private int accountID;
+	private String accountType;
+        private int accountNum;
+        private double accountBalance;
         
-        this.accountCreationDate = new Date();
+
+	public Account() {
+	}
+
+	public Account(int accountID, String accountType, int accountNum, double accountBalance) {
+		this.accountID = accountID;
+		this.accountType = accountType;
+                this.accountNum = accountNum;
+                this.accountBalance = accountBalance;
+	}
+
+    public int getAccountNum() {
+        return accountNum;
     }
 
-    public Account() {
+    public double getAccountBalance() {
+        return accountBalance;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getSortCode() {
-        return sortCode;
-    }
-
-    public void setSortCode(String sortCode) {
-        this.sortCode = sortCode;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public double getCurrentBalance() {
-        return currentBalance;
-    }
-
-    public void setCurrentBalance(double currentBalance) {
-        this.currentBalance = currentBalance;
-    }
-
-    public Date getAccountCreationDate() {
-        return accountCreationDate;
-    }
-
-    public void setAccountCreationDate(Date accountCreationDate) {
-        this.accountCreationDate = accountCreationDate;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
     }
     
     
-    
-    
-    
-    
+
+    public void setAccountNum(int accountNum) {
+        this.accountNum = accountNum;
+    }
+	
+	public int getAccountID() {
+		return accountID;
+	}
+
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
 }
